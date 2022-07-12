6e0bd9dee757c1b4bb299d610002f89ba7551803
NatnaelH <natnaelhabtamu0@gmail.com>
2022-07-12 10:39:07 +0300

    REST APIs for goals

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,24 +1,47 @@
+const { text } = require("express");
 const asyncHandler = require("express-async-handler");
+const Goal = require("../models/goalModel");
 
-const getGoals = asyncHandler((req, res, next) => {
-  res.status(200).json({ message: " get goals" });
+const getGoals = asyncHandler(async (req, res, next) => {
+  const goals = await Goal.find();
+  res.status(200).json(goals);
   // console.log(req.body);
 });
 
-const setGoal = asyncHandler((req, res, next) => {
+const setGoal = asyncHandler(async (req, res, next) => {
   // res.status(200).json({ message: " Set goal" });
   if (!req.body.text) {
     res.status(404);
     throw new Error("Please add a text field");
+  } else {
+    const goal = await Goal.create({
+      text: req.body.text,
+    });
+    res.status(200).json(goal);
   }
 });
 
-const updateGoal = asyncHandler((req, res, next) => {
-  res.status(200).json({ message: `update goal ${req.params.id}` });
+const updateGoal = asyncHandler(async (req, res, next) => {
+  const goalId = await Goal.findById(req.params.id);
+  if (!goalId) {
+    res.status(400);
+    throw new Error("Goal not found!");
+  }
+  const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+  res.status(200).json(updateGoal);
 });
 
-const deleteGoal = asyncHandler((req, res, next) => {
-  res.status(200).json({ message: `delete goal ${req.params.id}` });
+const deleteGoal = asyncHandler(async (req, res, next) => {
+  const goalId = await Goal.findById(req.params.id);
+  if (!goalId) {
+    res.status(400);
+    throw new Error("Goal not found!");
+  }
+  // await Goal.findByIdAndDelete(req.params.id);
+  await goalId.remove();
+  res.status(200).json({ id: req.params.id });
 });
 
 module.exports = { getGoals, setGoal, updateGoal, deleteGoal };
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/error-handler");
+const colors = require("colors");
 const port = process.env.PORT || 5000;
+const connectDB = require("./config/db");
+connectDB();
 const app = express();
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
