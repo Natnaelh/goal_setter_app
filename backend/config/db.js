3c08b1e578f924249e31696b5aaede5ed432df35
NatnaelH <natnaelhabtamu0@gmail.com>
2022-07-22 13:33:16 +0300

    User model, user routes,user controller, login and register endpoints and JWT authentication

--- backend/config/db.js
+++ backend/config/db.js
@@ -15,15 +15,3 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
-
-// mongoose.connect(process.env.MONGO_URI, {
-//     useNewUrlParser: true,
-//     // useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   });
-
-//   const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error: "));
-// db.once("open", function () {
-//   console.log("Connected successfully");
-// });
